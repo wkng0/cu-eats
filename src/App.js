cb1778757094701d41e00ca10fc182f908378bcc
Blackawabi <paucw0925@outlook.com>
2022-03-20 18:14:08 +0800

    update NoMatch route

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ import { Comment } from './comment';
 
 import './login.css';
 import {NavBar,FixedBottomNavigation,Footer} from './component'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{zIndex:9999 ,position:"relative"}}>
       <NavBar/>
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<LoginPage/>} />
+          <Route path="/login" element={<LoginPage/>} />
           <Route path="/comment" element={<Comment />} />
           <Route path="/about" element={<AboutUs/>}/>
+          <Route path="*" element={<NoMatch/>} />
         </Routes>
         <FixedBottomNavigation/>
       </BrowserRouter>
       
     </div>
+  );
+}
+
+function NoMatch() {
+  let location = useLocation();
+  return (
+      <div>
+          <h3>
+              No match for <code>{location.pathname}</code>
+          </h3>
+      </div>
   );
 }
 
