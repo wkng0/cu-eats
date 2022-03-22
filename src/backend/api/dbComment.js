fd16173642623d9c48d40216b170badb469a9379
Blackawabi <paucw0925@outlook.com>
2022-03-22 22:28:29 +0800

    update some new paths

--- src/backend/api/dbComment.js
+++ src/backend/api/dbComment.js
@@ -1,5 +1,11 @@
 import express from "express"
 import { MongoClient } from "mongodb";
+import bodyParser from "body-parser";
+import multer from "multer";
+import path from 'path'
+const __dirname=path.resolve();
+
+const upload=multer({dest:"comment/photo/"})
 const router = express.Router();
 
 const url="mongodb+srv://admin:admin_d2@groupd2.d3lwk.mongodb.net/sample_users?retryWrites=true&w=majority";
@@ -13,7 +19,7 @@ router.get("/", function(req, res) {
 
 
 let result="";
-router.get("/get:id", function(req, res) {
+router.get("/get/:id", function(req, res) {
     canteen=req.params.id;
     fetchComment(res)
     .then(console.log)
@@ -21,19 +27,58 @@ router.get("/get:id", function(req, res) {
     .finally(() => client.close());
 });
 
+router.use(bodyParser.urlencoded({extended: false}));
+router.post("/post/:id",function(req,res){
+    canteen=req.params.id;
+    postComment(req,res)
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+});
 
+
+router.post("/photo/post",upload.single("file"),function(req,res){
+    let data={
+        "filename":req.file.filename,
+        "filetype":req.file.mimetype
+    }
+    res.send(data)
+});
+
+router.get("/photo/get/:id",function(req,res){
+    console.log(__dirname)
+    res.sendFile(__dirname+"/comment/photo/"+req.params.id)
+});
+
+
+
+
+
+async function postComment(req,res){
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection(canteen);
+    const insertResult = await collection.insertOne({ 
+        userid: req.body['userid'],
+        title: req.body['title'],
+        description:req.body['description'],
+        type: req.body['type'],
+        datetime:new Date(),
+        image:req.body['image']
+    });
+    return "comment posted";
+};
 
 async function fetchComment(res){
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
     const collection = db.collection(canteen);
-    const commentList = await collection.find({}).toArray();
-    console.log('Found documents =>', commentList);
+    const commentList = await collection.find({}).sort({datetime: -1}).toArray();
+    //console.log('Found documents =>', commentList);
     res.send(commentList);
-    return "done";
-}
+    return "comments fetched";
+};
 
-
-
-export default router
+export default router;
