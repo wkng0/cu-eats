ca8f99dedad87466f78a8ea224537273d2be84d8
ccs-jason <80102310+ccs-jason@users.noreply.github.com>
2022-03-24 21:35:02 +0800

    Add files via upload

---
+++ src/NAmenu.js
@@ -0,0 +1,73 @@
+const NAmenu =
+[
+    {
+        "dishesID": "NA000A1",
+        "name" : "Cheese, Chill, Onions, Mustard, Rookwurst Sausage HotDog",
+        "prices": 40,
+        "category": "All-day Supply",
+        "image": "https://rs-menus-api.roocdn.com/images/f765f0c9-09b9-454a-bb7a-494eec979185/image.jpeg?width=1200&height=630&fit=crop",
+        "description": ""
+    },
+
+    {
+        "dishesID": "NA000A2",
+        "name" : "Melted Cheese, Corn, Mayonnaise, Franfurter Sausage HotDog",
+        "prices": 36,
+        "category": "All-day Supply",
+        "image": "https://spicysouthernkitchen.com/wp-content/uploads/Grilled-Cheese-Hot-Dogs-5.jpg",
+        "description": ""
+    },
+
+
+    {
+        "dishesID": "NA000A3",
+        "name" : "Fong-Jei Handmade Dumpings",
+        "prices": 46,
+        "category": "All-day Supply",
+        "image": "https://preview.redd.it/cd24u8471e131.jpg?width=960&crop=smart&auto=webp&s=cee7f5304b5cbe9968bd6f1e65253ba741cb3e75",
+        "description": ""
+    },
+
+
+    {
+        "dishesID": "NA000A4",
+        "name" : "Spicy Chicken, Pinapple, Ketchup, Jalapeno, Spicy Sauce",
+        "prices": 38,
+        "category": "All-day Supply",
+        "image": "https://www.mashed.com/img/gallery/copycat-burger-king-spicy-crispy-chicken-sandwich-recipe/intro-1620651447.webp",
+        "description": ""
+    },
+
+    {
+        "dishesID": "NA000A5",
+        "name" : "Spicy Chicken, Pinapple, Ketchup, Jalapeno, Spicy Sauce",
+        "prices": 38,
+        "category": "All-day Supply",
+        "image": "https://www.mashed.com/img/gallery/copycat-burger-king-spicy-crispy-chicken-sandwich-recipe/intro-1620651447.webp",
+        "description": ""
+    },
+
+    {
+        "dishesID": "NA000A6",
+        "name" : "Fong-Jei Handmade Dumpings Noodles",
+        "prices": 36,
+        "category": "All-day Supply",
+        "image": "https://i.pinimg.com/originals/49/74/55/497455d8f8c1b400a907a8777a0d8048.jpg",
+        "description": ""
+    },
+
+
+    {
+        "dishesID": "NA000A7",
+        "name" : "Braised Rice Noodle with Preserved Vegetable and Shredded Pork",
+        "prices": 32,
+        "category": "All-day Supply",
+        "image": "https://1.bp.blogspot.com/-17tX7nirKeU/YEuM--g6BuI/AAAAAAAA_7Y/XNoqPR8OP-QpC6HVzc013QzozSc3JV1cgCPcBGAsYHg/w640-h378/20210312_084604%257E2.jpg",
+        "description": ""
+    },
+
+
+    
+];
+
+export default NAmenu;
---
+++ src/Shawmeun.js
@@ -0,0 +1,80 @@
+const Shawmenu =
+[
+    {
+        "dishesID": "Shaw000All1",
+        "name" : "BBQ Prok Spaghetti + Fried Egg / Boiled Egg + Toast",
+        "prices": 32,
+        "category": "All-time Regular",
+        "image": "https://iamafoodblog.b-cdn.net/wp-content/uploads/2014/10/spaghetti-and-eggs-6w21.jpg",
+        "description": ""
+    },
+
+    {
+        "dishesID": "Shaw000All2",
+        "name" : "Mini BBQ Pork Rice + Fried Egg / Boiled Egg + Toast",
+        "prices": 32,
+        "category": "All-time Regular",
+        "image": "https://assets.bonappetit.com/photos/5de7e3e40b606000081843cf/5:4/w_3120,h_2496,c_limit/HLY-Egg%20Korma-16x9.jpg",
+        "description": ""
+    },
+    
+    {
+        "dishesID": "Shaw00Kong1",
+        "name" : "Kong Fu BBQ Pork Rice + Add-on Ingredient",
+        "prices": 29,
+        "category": "Coantoon Kong Fu Set",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/HK_Mongkok_Maxims_BBQ_Meat_Rice_Lunch_with_Green_vegetable.JPG/1200px-HK_Mongkok_Maxims_BBQ_Meat_Rice_Lunch_with_Green_vegetable.JPG",
+        "description": ""
+    },
+
+    {
+        "dishesID": "Shaw00Meat1",
+        "name" : "Signature Pan-fired Chicken Fillet with Tri-color Sauce with Chef's special sauce + Choice of Vgetables / Rice / Spaghetti",
+        "prices": 36,
+        "category": "Meaty Series",
+        "image": "https://static8.orstatic.com/userphoto2/photo/V/OJ2/04UFNB1EA72A7F2A3E0ADBpx.jpg",
+        "description": ""
+    },
+
+    {
+        "dishesID": "Shaw00Meat2",
+        "name" : "Deep-fried Chicken Thigh Rice",
+        "prices": 32,
+        "category": "Meaty Series",
+        "image": "https://previews.123rf.com/images/gbh007/gbh0071408/gbh007140801970/30864182-チャーハンと唐揚げの上から見る.jpg",
+        "description": ""
+    },
+
+    {
+        "dishesID": "Shaw00A1",
+        "name" : "Taiwanese Braised Pork and Braised Egg Rice",
+        "prices": 26,
+        "category": "Asian Delights",
+        "image": "https://redhousespice.com/wp-content/uploads/2021/05/Lu-Rou-Fan-braised-pork-with-rice.jpg",
+        "description": ""
+    },
+
+    {
+        "dishesID": "Shaw000B1",
+        "name" : "US Style Pan-fried Sirloin Steak Rice with Black Pepper",
+        "prices": 36,
+        "category": "Beef Jumbo Series",
+        "image": "https://static6.orstatic.com/userphoto2/photo/16/XAH/06KQJL12880874403A247Cpx.jpg",
+        "description": ""
+    },
+
+    {
+        "dishesID": "Shaw000T1",
+        "name" : "Ham, Sausage & Fried Egg Rice (in Soy Sauce)",
+        "prices": 22,
+        "category": "Tasty Option",
+        "image": "https://static6.orstatic.com/userphoto2/photo/16/XAH/06KQJL12880874403A247Cpx.jpg",
+        "description": ""
+    },
+
+   
+
+
+];
+
+export default Shawmenu;
