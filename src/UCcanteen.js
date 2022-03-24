eacdc9f76729afe1ab60690e1c13be4adcd5e08b
ccs-jason <80102310+ccs-jason@users.noreply.github.com>
2022-03-24 21:39:37 +0800

    Update UCcanteen.js

--- src/UCcanteen.js
+++ src/UCcanteen.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import './canteen.css';
+import UCmenu from './UCmenus';
+// import ShowUCDishes from './ShowUCDishes';
+// import { Modal } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 
-function UCCanteen() {
+function ShowUCDishes({UCmenu}) {
+
+
+    return(
+        <>
+        <div style={{margin: '70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
+
+            <div>
+                <h1>{UCmenu.name}</h1>
+                <img src={UCmenu.image} className="img-fluid" style={{height: '200px' , width: '200px'}}/>
+            </div>
+
+        </div>
+        </> 
+    );
+}
+
+export default function UCCanteen() {
+    // const [show, setShow] = useState(false);
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
     return(
         <>
         <section class="UCheader">
@@ -14,23 +38,21 @@ function UCCanteen() {
             </div>
         </section>
 
-        {/* <!-- ----- restaurant ------ --> */}
-        <section class="Salad">
-            <h1>Salad We Offer</h1>
-            <p>Lorem ipsum sit amet, consectetur adipiscine eilt.</p>
-
-            <div class="row-1">
-                <div class="Salad-col">
-                    <h3>Salad</h3>
-                    <p>The College Staff and Student Canteens are located on the G/F of the Cheung Chuk Shan Amenities Building. 
-                        The total floor area is about 570 square metres and it can serve up to 360 people.  
-                        The College canteens are now operated by Joyful Inn Limited.  
-                    </p>
-                </div>
-            </div> 
-        </section>
+        <div>
+            <div className="row">
+                {UCmenu.map(UCmenu =>{
+                    return(
+                        <div>
+                            <ShowUCDishes UCmenu={UCmenu}/>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
         </>
-    )
+    );
 }
 
-export default UCCanteen;
+
+
+// export default UCCanteen;
