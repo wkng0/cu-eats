ae6a3d6c94705f4b126785c2aad9bd740d9d53c5
ccs-jason <80102310+ccs-jason@users.noreply.github.com>
2022-03-25 00:39:24 +0800

    Update ShawCanteen.js

--- src/ShawCanteen.js
+++ src/ShawCanteen.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './canteen.css';
 import Shawmenu from './Shawmeun';
 
+
 function ShowShawDishes({Shawmenu}) {
 
+    const [varientsh, setvarientsh] = useState('small');
+    const [quantitysh, setquantityshsh] = useState(1);
 
     return(
         <>
@@ -12,13 +15,43 @@ function ShowShawDishes({Shawmenu}) {
             <div>
                 <h1>{Shawmenu.name}</h1>
                 <img src={Shawmenu.image} className="img-fluid" style={{height: '200px' , width: '200px'}}/>
+                {/* <h3>${NAmenu.prices}</h3> */}
+                <div className='w-100 m-1'>
+                    <p>Varients</p>
+                        <select className='form-control' value={varientsh} onChange={(e)=> {setvarientsh(e.target.value)}}>
+                            {Shawmenu.varients.map(varientsh=>{
+                                return <option value={varientsh}>{varientsh}</option>
+                            })} 
+                        </select>
+                </div>
+
+                <div className='w-100 m-1'>
+                    <p>Quality</p>
+                    <select className='form-control' value={quantitysh} onChange={(e)=>{setquantityshsh(e.target.value)}}>
+                        {[...Array(10).keys()].map((x, i)=> {
+                            return <option value={i+1}>{i}</option>
+                        })}
+                    </select>
+                </div>
+
+                <div className='flex-container'>
+                    <div className='m-1 w-100'>
+                        <h1 className='mt-1'>Price: ${Shawmenu.prices[0][varientsh]*(quantitysh-1)}</h1>
+                    </div>
+
+                    <div className='m-1 w-100'>
+                        <button className='btn'>ADD TO CART</button>
+                    </div>
+                </div>
+
             </div>
+
+            
 
         </div>
         </> 
     );
 }
-
 
 function ShawCanteen() {
     return(
