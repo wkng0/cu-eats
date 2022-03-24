345e3599889ae3f173f9699c38af61b3c5f2e667
yylam0 <79801535+yylam0@users.noreply.github.com>
2022-03-25 00:59:11 +0800

    Update cart.js

--- src/cart.js
+++ src/cart.js
@@ -1,14 +1,72 @@
-import { Button } from "@material-ui/core";
-import { CartItemType } from "../Menu";
-import CartItem from "../CartItem/CartItem";
-import styled from "styled-components";
+import * as React from 'react';
+import { Button } from '@mui/material';
+//import { CartItemType } from "../Menu";
+import cartItems from "./cart.json";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-export const Wrapper = styled.aside`
-  width: 500px;
-  padding: 20px;
-`;
- 
+
+/*
+const Props = {
+  item: CartItemType,
+  addToCart: (clickedItem={CartItemType}),
+  removeFromCart: (id={number})
+};
+*/
+
+
+
+const CartItem = ({ item }) => {
+  return (    
+    <div>
+      <TableRow >
+        {item.image && <TableCell rowSpan={2} sx={{pr: 0}}><img src={item.image} alt={item.title} width='100' height='100'/></TableCell> }
+        <TableCell colSpan={2} sx={{borderBottom: "none", pb: 0, pl: item.image? 1 : 3}}>
+          <h4 style={{color: '#5D4E99'}}>{item.title}</h4>
+          <p style={{color: '#707070'}}>{item.description}</p>
+        </TableCell> 
+      </TableRow>
+      <TableRow>
+        <TableCell sx={{ width: item.image? "50%" : "70%", pt: 0, pb: 0, pl: item.image? 1 : 3}}>
+          <p>Price: ${item.price.toFixed(1)}</p>
+        </TableCell>
+        <TableCell sx={{ width: '30%', pt: 0}}>
+          <div className='container'>
+            <Button 
+              size="small" 
+              //onClick={(e) => item.amount -= 1}
+              sx={{bgcolor: '#F4CB86', color: '#5D4E99', ':hover': {bgcolor:'#ffe082'}}}
+            >
+              -
+            </Button>
+            <Button disabled sx={{':disabled':{color: 'black'}}}>{item.amount}</Button>
+            <Button 
+              size="small" 
+              //onClick={(e) => item.amount += 1}
+              sx={{bgcolor: '#F4CB86', color: '#5D4E99', ':hover': {bgcolor:'#ffe082'}}}
+            >
+              +
+            </Button>
+          </div>
+        </TableCell>
+      </TableRow>
+    </div>
+  );
+};
+
 class Cart  extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { 
+        total: 0,
+    };
+}
+/*
 var Props = {
   cartItems: CartItemType[];
   addToCart: (clickedItem: CartItemType) => void;
@@ -16,22 +74,37 @@ var Props = {
 };
 
 const Cart = ({ cartItems, addToCart, removeFromCart }: Props) => {
-  const calculateTotal = (items: CartItemType[]) =>
-    items.reduce((acc, item) => acc + item.amount * item.price, 0);
+*/
 
-  return (
-    <Wrapper>
-      <h2>Your Cart</h2>
-      {cartItems.length === 0 ? <p>No items in cart.</p> : null}
-      {cartItems.map((item) => (
-        <CartItem
-          key={item.id}
-          item={item}
-          addToCart={addToCart}
-          removeFromCart={removeFromCart}
-        />
-      ))}
-      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
-    </Wrapper>
-  );
+  addTotal(item, e){ 
+    this.setState({total: this.state.total + item.price * item.amount});
+  }
+
+  render(){
+    return (
+      <div>
+        <h2 className='text-center'>Shopping Cart</h2>
+        {cartItems.cartItems.length === 0 ? <p>No items in cart.</p> : null}
+       
+          <Table style={{ tableLayout: "auto" }} aria-label="spanning table" padding='normal'>
+            {cartItems.cartItems.map((item) => (
+              <TableRow >
+              <CartItem
+                key={item.id}
+                item={item}
+                //addToCart={addToCart}
+                //removeFromCart={removeFromCart}
+              />
+              
+              </TableRow>
+            ))}
+            <TableRow >
+              <h2>Total: ${this.state.total.toFixed(1)}</h2>
+            </TableRow>
+          </Table>
+      </div>
+    );
+  }
 };
+
+export {Cart};
