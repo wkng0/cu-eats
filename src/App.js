881476e6ef8f6680db84fbc46d17c57438b7f1b9
yylam0 <79801535+yylam0@users.noreply.github.com>
2022-03-25 20:30:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
       <NavBar/>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<HomePage/>}/>
           <Route path="/login" element={<LoginPage/>} />
           <Route path="/comment" element={<Comment />} />
-          <Route path="/about" element={<HomePage/>}/>
           <Route path="/profile" element={<Profile/>} />
           <Route path="/profile/account" element={<Account/>} />
           <Route path="/profile/address" element={<Address/>} />
