3ebd4e26b3f885c4831c34724f0b5c1246260ab1
wkng0 <wingkiv22@gmail.com>
2022-03-29 20:29:19 +0800

    update Register

--- src/backend/api/dbAccount.js
+++ src/backend/api/dbAccount.js
@@ -10,7 +10,10 @@ const url="mongodb+srv://admin:admin_d2@groupd2.d3lwk.mongodb.net/sample_users?r
 const client=new MongoClient(url);
 const dbName="Account";
 const space=""
-let user_email="";
+let user_email = "";
+let user_name = "";
+
+router.use(bodyParser.urlencoded({extended: false}));
 
 router.get("/", function(req, res) {
     res.send("API is working properlyyyyyyyyyy");
@@ -26,23 +29,41 @@ router.get("/exist/:email", function(req, res) {
     .finally(() => client.close());
 });
 
-// router.get("/get/:email", function(req, res) {
-//     user_email = req._construct.params.email
-//     veriAcc(user_email)
-//     .then(console.log)
-//     .catch(console.error)
-//     .finally(() => client.close());
-// });
+router.post("/createAccount",function(req,res){
+    addUser(req,res)
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+});
 
-router.use(bodyParser.urlencoded({extended: false}));
+router.get("/userName/:name",function(req,res){
+    user_name=req.params.name;
+    veriUserName(res)
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+});
 
-router.get("/get/:id",function(req,res){
-    user_email=req.params.id;
+router.get("/get/:email",function(req,res){
+    user_email=req.params.email;
     fetchAccount(res)
     .then(console.log)
     .catch(console.error)
     .finally(() => client.close());
 });
+
+async function veriUserName(res){
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection("Info");
+    let result = await collection.countDocuments({"user_name":user_name});
+    if(result == 0){
+        res.json({"unique":"true"});
+    }else{
+        res.json({"unique":"false"});
+    }
+};
 
 async function veriAcc(res){
     await client.connect();
@@ -66,5 +87,27 @@ async function fetchAccount(res){
     res.send(result);
     
 };
+
+
+async function addUser(req,res){
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection("Info");
+    const insertResult = await collection.insertOne({ 
+        email: req.body['email'],
+        password: req.body['password'],
+        user_name:req.body['user_name'],
+        first_name: req.body['first_name'],
+        last_name: req.body['last_name'],
+        point: 0,
+        phone: req.body['phone'],
+        college:req.body['college'],
+        faculty:req.body['faculty'],
+        gender: req.body['gender'],
+    });
+    return "comment posted";
+};
+
 
 export default router;
