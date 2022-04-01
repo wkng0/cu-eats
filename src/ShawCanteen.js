910644d1744cc75cc47ebb4ae086df95ee2cdb9b
ccs-jason <80102310+ccs-jason@users.noreply.github.com>
2022-04-01 18:26:38 +0800

    Update ShawCanteen.js

--- src/ShawCanteen.js
+++ src/ShawCanteen.js
@@ -1,62 +1,34 @@
 import React, {useState} from 'react';
+import {useEffect} from "react";
 import './canteen.css';
-import Shawmenu from './Shawmeun';
+// import menu from './menu';
+// import Select from 'react-select';
+import Axios from "axios"; 
 
-
-function ShowShawDishes({Shawmenu}) {
-
-    const [varientsh, setvarientsh] = useState('small');
-    const [quantitysh, setquantityshsh] = useState(1);
-
-    return(
-        <>
-        <div style={{margin: '70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
-
-            <div>
-                <h1>{Shawmenu.name}</h1>
-                <img src={Shawmenu.image} className="img-fluid" style={{height: '200px' , width: '200px'}}/>
-                {/* <h3>${NAmenu.prices}</h3> */}
-                <div className='w-100 m-1'>
-                    <p>Varients</p>
-                        <select className='form-control' value={varientsh} onChange={(e)=> {setvarientsh(e.target.value)}}>
-                            {Shawmenu.varients.map(varientsh=>{
-                                return <option value={varientsh}>{varientsh}</option>
-                            })} 
-                        </select>
-                </div>
-
-                <div className='w-100 m-1'>
-                    <p>Quality</p>
-                    <select className='form-control' value={quantitysh} onChange={(e)=>{setquantityshsh(e.target.value)}}>
-                        {[...Array(10).keys()].map((x, i)=> {
-                            return <option value={i+1}>{i}</option>
-                        })}
-                    </select>
-                </div>
-
-                <div className='flex-container'>
-                    <div className='m-1 w-100'>
-                        <h1 className='mt-1'>Price: ${Shawmenu.prices[0][varientsh]*(quantitysh-1)}</h1>
-                    </div>
-
-                    <div className='m-1 w-100'>
-                        <button className='btn'>ADD TO CART</button>
-                    </div>
-                </div>
-
-            </div>
-
-            
-
-        </div>
-        </> 
-    );
-}
 
 function ShawCanteen() {
+
+    const [varient, setvarient] = useState('small');
+    const [quantity, setquantity] = useState(1);
+
+    const [listOfMenu, setListOfMenu] = useState([]);
+    const [name] = useState("");
+    const [varients] = useState([]);
+    const [prices] = useState([]);
+    const [username] = useState("");
+
+
+    // make api call 
+    useEffect(() => {
+        Axios.get("http://localhost:7000/dbMenu/getMenu/ShawMenu").then((response) => {
+            setListOfMenu(response.data)
+        });
+    }, []);
+
+
     return(
         <>
-        <section class="Shawheader">
+         <section class="Shawheader">
             <div class="text-box">
                     <h1>Shaw Canteen</h1>
                     <p>SeeYou@Shaw是一個結合良朋好友和優質餐飲，並使你足以自豪的理想社群。
@@ -71,10 +43,50 @@ function ShawCanteen() {
         {/* <!-- ----- restaurant ------ --> */}
         <div>
             <div className="row">
-                {Shawmenu.map(Shawmenu =>{
+                {listOfMenu.map( (menu) =>{
                     return(
                         <div>
-                            <ShowShawDishes Shawmenu={Shawmenu}/>
+                            {/* <ShowNADishes menu={menu}/> */}
+                             <div style={{margin: '70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
+
+                                <div>
+                                    <h1>{menu.name}</h1>
+                                    <img src={menu.image} className="img-fluid" style={{height: '200px' , width: '200px'}}/>
+                                    {/* <h3>${menu.prices}</h3> */}
+                                    <div className='w-100 m-1'>
+                                        <p>Varients</p>
+                                            <select className='form-control' value={varient} onChange={(e)=> {setvarient(e.target.value)}}>
+                                                {menu.varients.map(varient=>{
+                                                    return <option value={varient}>{varient}</option>
+                                                })} 
+                                            </select>
+                                    </div>
+
+                                    <div className='w-100 m-1'>
+                                        <p>Quality</p>
+                                        <select className='form-control' value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
+                                            {[...Array(10).keys()].map((x, i)=> {
+                                                return <option value={i+1}>{i}</option>
+                                            })}
+                                        </select>
+                                    </div>
+
+                                    <div className='flex-container'>
+                                        <div className='m-1 w-100'>
+                                            <h1 className='mt-1'>Price: ${menu.prices[0][varient]*(quantity-1)}</h1>
+                                        </div>
+
+                                        <div className='m-1 w-100'>
+                                            <button className='btn'>ADD TO CART</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                
+
+                            </div>
+
                         </div>
                     );
                 })}
