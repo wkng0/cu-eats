78fca83b919b062bfacc01679a9e71aec4e826ff
ccs-jason <80102310+ccs-jason@users.noreply.github.com>
2022-04-01 18:28:52 +0800

    Update UCcanteen.js

--- src/UCcanteen.js
+++ src/UCcanteen.js
@@ -1,63 +1,31 @@
 import React, {useState} from 'react';
+import {useEffect} from "react";
 import './canteen.css';
-import UCmenu from './UCmenus';
-// import ShowUCDishes from './ShowUCDishes';
-// import { Modal } from 'react-bootstrap';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+// import menu from './menu';
+// import Select from 'react-select';
+import Axios from "axios"; 
 
-function ShowUCDishes({UCmenu}) {
+
+function UCCanteen() {
 
     const [varient, setvarient] = useState('small');
     const [quantity, setquantity] = useState(1);
 
-    return(
-        <>
-        <div style={{margin: '70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
-
-            <div>
-                <h1>{UCmenu.name}</h1>
-                <img src={UCmenu.image} className="img-fluid" style={{height: '200px' , width: '200px'}}/>
-                
-            </div>
-
-            <div className='w-100 m-1'>
-                    <p>Varients</p>
-                        <select className='form-control' value={varient} onChange={(e)=> {setvarient(e.target.value)}}>
-                            {UCmenu.varients.map(varient=>{
-                                return <option value={varient}>{varient}</option>
-                            })} 
-                        </select>
-                </div>
-
-            <div className='w-100 m-1'>
-                    <p>Quality</p>
-                    <select className='form-control' value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
-                        {[...Array(10).keys()].map((x, i)=> {
-                            return <option value={i+1}>{i}</option>
-                        })}
-                    </select>
-                </div>
-
-                <div className='flex-container'>
-                    <div className='m-1 w-100'>
-                        <h1 className='mt-1'>Price: ${UCmenu.prices[0][varient]*(quantity-1)}</h1>
-                    </div>
-
-                    <div className='m-1 w-100'>
-                        <button className='btn'>ADD TO CART</button>
-                    </div>
-                </div>
+    const [listOfMenu, setListOfMenu] = useState([]);
+    const [name] = useState("");
+    const [varients] = useState([]);
+    const [prices] = useState([]);
+    const [username] = useState("");
 
 
-        </div>
-        </> 
-    );
-}
+    // make api call 
+    useEffect(() => {
+        Axios.get("http://localhost:7000/dbMenu/getMenu/UCMenu").then((response) => {
+            setListOfMenu(response.data)
+        });
+    }, []);
 
-export default function UCCanteen() {
-    // const [show, setShow] = useState(false);
-    // const handleClose = () => setShow(false);
-    // const handleShow = () => setShow(true);
+
     return(
         <>
         <section class="UCheader">
@@ -70,21 +38,60 @@ export default function UCCanteen() {
             </div>
         </section>
 
+        {/* <!-- ----- restaurant ------ --> */}
         <div>
             <div className="row">
-                {UCmenu.map(UCmenu =>{
+                {listOfMenu.map( (menu) =>{
                     return(
                         <div>
-                            <ShowUCDishes UCmenu={UCmenu}/>
+                            {/* <ShowNADishes menu={menu}/> */}
+                             <div style={{margin: '70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
+
+                                <div>
+                                    <h1>{menu.name}</h1>
+                                    <img src={menu.image} className="img-fluid" style={{height: '200px' , width: '200px'}}/>
+                                    {/* <h3>${menu.prices}</h3> */}
+                                    <div className='w-100 m-1'>
+                                        <p>Varients</p>
+                                            <select className='form-control' value={varient} onChange={(e)=> {setvarient(e.target.value)}}>
+                                                {menu.varients.map(varient=>{
+                                                    return <option value={varient}>{varient}</option>
+                                                })} 
+                                            </select>
+                                    </div>
+
+                                    <div className='w-100 m-1'>
+                                        <p>Quality</p>
+                                        <select className='form-control' value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
+                                            {[...Array(10).keys()].map((x, i)=> {
+                                                return <option value={i+1}>{i}</option>
+                                            })}
+                                        </select>
+                                    </div>
+
+                                    <div className='flex-container'>
+                                        <div className='m-1 w-100'>
+                                            <h1 className='mt-1'>Price: ${menu.prices[0][varient]*(quantity-1)}</h1>
+                                        </div>
+
+                                        <div className='m-1 w-100'>
+                                            <button className='btn'>ADD TO CART</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                
+
+                            </div>
+
                         </div>
                     );
                 })}
             </div>
         </div>
         </>
-    );
+    )
 }
 
-
-
-// export default UCCanteen;
+export default UCCanteen;
