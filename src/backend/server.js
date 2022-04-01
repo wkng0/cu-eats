3beb305b18300626db062642fa065731ac0ffbb9
ccs-jason <80102310+ccs-jason@users.noreply.github.com>
2022-04-01 16:50:26 +0800

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import user from "./api/users.route.js"
 import comment from "./api/dbComment.js"
 import account from "./api/dbAccount.js"
+import menu from "./api/dbMenu.js"
 
 const app = express();
 const port = process.env.PORT || 7000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/v1/users", user);
 app.use("/dbAccount",account);
 app.use("/dbComment",comment);
+app.use("/dbMenu", menu);
 app.use("*", (req, res) => res.status(404).json({ error: "not found"}));
 
 app.listen(port, () => {
