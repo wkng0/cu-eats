3818559386baae93f9d51bb7f48c4ed8425b0aaa
ccs-jason <80102310+ccs-jason@users.noreply.github.com>
2022-04-01 21:13:51 +0800

    Update NAcanteen.js

--- src/NAcanteen.js
+++ src/NAcanteen.js
@@ -8,14 +8,9 @@ import Axios from "axios";
 
 function NACanteen() {
 
-    const [varient, setvarient] = useState('small');
-    const [quantity, setquantity] = useState(1);
+  
 
     const [listOfMenu, setListOfMenu] = useState([]);
-    const [name] = useState("");
-    const [varients] = useState([]);
-    const [prices] = useState([]);
-    const [username] = useState("");
 
 
     // make api call 
@@ -41,50 +36,10 @@ function NACanteen() {
         {/* <!-- ----- restaurant ------ --> */}
         <div>
             <div className="row">
-                {listOfMenu.map( (NAmenu) =>{
+                {listOfMenu.map( (menu) =>{
                     return(
                         <div>
-                            {/* <ShowNADishes NAmenu={NAmenu}/> */}
-                             <div style={{margin: '70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
-
-                                <div>
-                                    <h1>{NAmenu.name}</h1>
-                                    <img src={NAmenu.image} className="img-fluid" style={{height: '200px' , width: '200px'}}/>
-                                    {/* <h3>${NAmenu.prices}</h3> */}
-                                    <div className='w-100 m-1'>
-                                        <p>Varients</p>
-                                            <select className='form-control' value={varient} onChange={(e)=> {setvarient(e.target.value)}}>
-                                                {NAmenu.varients.map(varient=>{
-                                                    return <option value={varient}>{varient}</option>
-                                                })} 
-                                            </select>
-                                    </div>
-
-                                    <div className='w-100 m-1'>
-                                        <p>Quantity</p>
-                                        <select className='form-control' value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
-                                            {[...Array(10).keys()].map((x, i)=> {
-                                                return <option value={i+1}>{i+1}</option>
-                                            })}
-                                        </select>
-                                    </div>
-
-                                    <div className='flex-container'>
-                                        <div className='m-1 w-100'>
-                                            <h1 className='mt-1'>Price: ${NAmenu.prices[0][varient]*(quantity)}</h1>
-                                        </div>
-
-                                        <div className='m-1 w-100'>
-                                            <button className='btn'>ADD TO CART</button>
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                                
-
-                            </div>
-
+                            <ShowDishes menu={menu}/>
                         </div>
                     );
                 })}
@@ -92,6 +47,62 @@ function NACanteen() {
         </div>
         </>
     )
+}
+
+
+function ShowDishes({menu}) {
+    const [varient, setvarient] = useState('small');
+    const [quantity, setquantity] = useState(1);
+
+    const [name] = useState("");
+    const [varients] = useState([]);
+    const [prices] = useState([]);
+    const [username] = useState("");
+
+    return(
+        <div>
+        {/* <ShowNADishes NAmenu={NAmenu}/> */}
+            <div style={{margin: '70px'}} className='shadow-lg p-3 mb-5 bg-white rounded'>
+
+            <div>
+                <h1>{menu.name}</h1>
+                <img src={menu.image} className="img-fluid" style={{height: '200px' , width: '200px'}}/>
+                {/* <h3>${NAmenu.prices}</h3> */}
+                <div className='w-100 m-1'>
+                    <p>Varients</p>
+                        <select className='form-control' value={varient} onChange={(e)=> {setvarient(e.target.value)}}>
+                            {menu.varients.map(varient=>{
+                                return <option value={varient}>{varient}</option>
+                            })} 
+                        </select>
+                </div>
+
+                <div className='w-100 m-1'>
+                    <p>Quantity</p>
+                    <select className='form-control' value={quantity} onChange={(e)=>{setquantity(e.target.value)}}>
+                        {[...Array(10).keys()].map((x, i)=> {
+                            return <option value={i+1}>{i+1}</option>
+                        })}
+                    </select>
+                </div>
+
+                <div className='flex-container'>
+                    <div className='m-1 w-100'>
+                        <h1 className='mt-1'>Price: ${menu.prices[0][varient]*(quantity)}</h1>
+                    </div>
+
+                    <div className='m-1 w-100'>
+                        <button className='btn'>ADD TO CART</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    );
+
+
 }
 
 export default NACanteen;
