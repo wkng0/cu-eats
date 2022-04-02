08e176e15a2eedac0ed373635b34acac0b215bd5
HillaryTam <hillarytam1024@gmail.com>
2022-04-02 14:57:08 +0800

    add shopping cart files

---
+++ src/shoppingCart/sc-data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      id: 1,
+      title: 'Food 1',
+      price: 69.9,
+      img:
+        '../image/fast_food_1.png',
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: 'Food 2',
+      price: 79.9,
+      img:
+        '../image/fast_food_2.png',
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: 'Food 3',
+      price: 79.3,
+      img:
+        '/image/fast_food_3.png',
+      amount: 1,
+    },
+  ];
