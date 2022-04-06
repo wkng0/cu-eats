539331885a2997de987ad0a45c5c4ffafa8bf894
Blackawabi <paucw0925@outlook.com>
2022-04-06 22:30:29 +0800

    add dish

--- src/AddDishes.js
+++ src/AddDishes.js
@@ -1,52 +1,41 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import Axios from "axios";
+import { Button } from "@mui/material";
 
 
 export default function AddDishes() {
 
     const [name, setName] = useState('');
-    const [variant, SetListOfvariant] = useState('');
+    const [variant, setVariant] = useState('');
+    const [variantList, setVariantList]=useState([]);
     const [Regularprices, setRegularprices] = useState(0);
     const [category, setCategory] = useState('');
     const [image, setImage] = useState('');
-    // const [tempvariant, setTempvariant] = useState([]);
-    // const [tempprices, setTempPrices] = useState([]);
-
 
     const AddDishes = () => {
         Axios.post('http://localhost:7000/dbNewMenu/AddMenu/NaMenu', {
           name: name, 
-          varients: variant,
+          varients: variantList,
           prices: Regularprices,
           category: category,
           image: image,
         })
     };
 
-
-    // function VariantList(props) {
-    //     const [checked, setChecked] = useState([]);
-
-    //     useEffect(() => {
-
-    //     })
-
-    //     const handleToggle = (value) => () => {
-    //         const currentIndex = checked.indexOf(value);
-    //         const newChecked = [...checked];
-
-    //         if (currentIndex === -1) {
-    //             newChecked.push(value);
-    //         } else {
-    //             newChecked.splice(currentIndex, 1); 
-    //         }
-        
-    //     setChecked(newChecked); 
-    // }};
-
+    const saveVariant=()=>{
+        let newVariantList=variantList;
+        newVariantList.push(variant);
+        setVariantList(newVariantList);
+        let newVariant=variant;
+        let node=document.createTextNode(newVariant);
+        let para= document.createElement("p");
+        para.appendChild(node);
+        document.getElementById("variantList").appendChild(para);
+        setVariant("");
+    }
     return (
-        <div>
+        <div className="container">
             <div>
                 <h1>Add Dish</h1>
 
@@ -62,44 +51,20 @@ export default function AddDishes() {
                             setName(event.target.value)
                         }}
                     />
-
-                    {/* <input 
-                        className="form-control"
-                        type="text" 
-                        placeholder="Variant"
-                        value={variant}
-                        onChange={(event)=> {
-                            let data = tempvariant;
-                            data.push(event.target.value);
-                            setTempvariant(data);
-                            SetListOfvariant(event.target.value);
-                        }}
-                    />
-
-                    <input 
-                        className="form-control"
-                        type="number" 
-                        placeholder="Regular Prices"
-                        value={Regularprices}
-                        onChange={(event)=> {
-                            let data = tempprices;
-                            data.push(event.target.value);
-                            setTempPrices(data);
-                            setRegularprices(event.target.value);
-                        }}
-
-                    /> */}
-
+                    
                     <input 
                         className="form-control"
                         type="text" 
                         placeholder="Variant"
                         value={variant}
                         onChange={(event)=> {
-                            SetListOfvariant(event.target.value)
+                            setVariant(event.target.value)
                         }}
                     />
+                    <Button variant="contained" onClick={saveVariant}>+</Button>
+                    <div id="variantList">
 
+                    </div>
                     <input 
                         className="form-control"
                         type="number" 
