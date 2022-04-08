4e3e72e03b9ac627663cf7815a3f9212c1841496
yylam0 <79801535+yylam0@users.noreply.github.com>
2022-04-09 00:04:17 +0800

    Update receipt.js

--- src/receipt.js
+++ src/receipt.js
@@ -27,15 +27,37 @@ function Receipt() {
     const [phone, setPhone] = React.useState('');
     const [receiptID, setReceiptID] = React.useState('');
     const [res, setRestaurant] = React.useState('');
-    const [item, setItem] = React.useState(null);
+    const [orderItem, setItem] = React.useState(null);
     const [point, setPoint] = React.useState(0);
     const [discount, setDiscount] = React.useState(0);
     const [subtotal, setSubtotal] = React.useState(0);
+    const [timestamp, setTime] = React.useState(0);
     const [total, setTotal] = React.useState(0);
     const [address, setAddress] = React.useState(null);
     const [cutlery, setCutlery] = React.useState(true);
     const [status, setStatus] = React.useState(false);
     const [fetchFinish, setFetch] = React.useState(false);
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: '2-digit',day: '2-digit' ,hour: '2-digit', minute: '2-digit'}
+        return new Intl.DateTimeFormat('en-US', options).format(dateString);
+    }
+    const formatTime = (dateString) => {
+        const options = { hour: '2-digit', minute: '2-digit'}
+        return new Intl.DateTimeFormat('en-US', options).format(dateString);
+    }
+    const handleTaken = () => {setStatus(true);};
+    const handleCutlery = () => {
+        if (cutlery) {
+            return (
+                <Grid container sx ={{color: '#707070'}}>
+                    <Grid item xs={1} sx={{color: '#5D4E99'}}><b>{cutleryNo}</b></Grid>
+                    <Grid item xs={9}>x &nbsp;Cutlery</Grid>
+                    <Grid item xs={1} sx={{textAlign:'right'}}>$</Grid>
+                    <Grid item xs={1} sx={{textAlign:'right'}}>0.0</Grid>
+                </Grid>
+            )
+        }
+    }
 
     React.useEffect(()=>{
         fetch("http://localhost:7000/dbReceipt/get/"+window.location.pathname.replace('/receipt/',''))
@@ -48,16 +70,23 @@ function Receipt() {
             setPhone(data[0].phone);
             setAddress(data[0].address);
             setCutlery(data[0].cutlery)
-            setItem(data[0].item);
+            setItem(JSON.parse(data[0].item));
             setSubtotal(data[0].subtotal);
             setDiscount(data[0].discount);
             setTotal(data[0].total);
             setStatus(data[0].status);
+            setTime(data[0].timestamp);
+            setFetch(true);
+            console.log(orderItem[0])
+            console.log(formatDate(timestamp))
+            console.log(formatTime(timestamp))
         })
         .then(console.log("Successfully get to receipt info"))
         .catch(err=>{console.log(err);})
     },[fetchFinish])
 
+    let cutleryNo = 0;
+    if (fetchFinish) {
     if (status == false) {
     return (
         <>
@@ -71,16 +100,18 @@ function Receipt() {
             </Button>
         </div>
         <br/>
-        <h3 style={{color: '#5D4E99'}}>Your Order <span style={{color: '#FFC107'}}>{receiptID}</span></h3><br/>
+        <h3 style={{color: '#5D4E99'}}>Receipt <span style={{color: '#FFC107'}}>{receiptID}</span></h3><br/>
         <div style={{textAlign: 'center'}}>
+            <h5 style={{color: '#5D4E99'}}>Order time: {formatDate(timestamp)}</h5>
+            <h5 style={{color: '#5D4E99'}}>Expected delivery time: {formatTime(timestamp+900000)} - {formatTime(timestamp+1800000)}</h5><br/>
             <h5 style={{color: '#5D4E99'}}>Name: {name}&nbsp;&nbsp;&nbsp;Tel: {phone}</h5>
             <h5 style={{color: '#5D4E99'}}>Address: {address}</h5>
         </div>
          
         <Table style={{width:'80%', margin:'auto', maxWidth:650}} aria-label="spanning table" padding='normal'>
             <br/><Divider /><br/> 
-            {/*Cart.cartItems.map((item) => (
-                total += item.amount * item.price,
+            {orderItem.map((item) => (
+                cutleryNo += item.amount,
                 <>
                 <Grid container sx ={{color: '#707070'}}>
                     <Grid item xs={1} sx={{color: '#5D4E99'}}><b>{item.amount}</b></Grid>
@@ -93,42 +124,45 @@ function Receipt() {
                     <Grid item xs={11}><pre>{item.description && '   - '}{item.description}</pre></Grid>
                 </Grid>
                 </>
-            ))*/}
-            <h6><DiningIcon sx={{color:'#5D4E99'}}/>&nbsp;{cutlery? 'Cutlery needed':'Cutlery not needed'}</h6>
+            ))}
+            {handleCutlery()}
             <br/><Divider /><br/>          
-            <h4 style={{color: '#5D4E99'}}>Payment</h4>
             <Grid container>
                 <Grid item xs={9}>Points Rebate</Grid>
-                <Grid item xs={3} style ={{textAlign:'right', color:'#5D4E99'}}>{~~(total/50)*5} Points</Grid>
+                <Grid item xs={3} style ={{textAlign:'right', color:'#5D4E99'}}>{point} Points</Grid>
             </Grid><br/>
             <Grid container>
                 <Grid item xs={10}>Subtotal</Grid>
-                <Grid item xs={2} sx ={{textAlign:'right', color: '#5D4E99'}}><b>${total.toFixed(1)}</b></Grid>
+                <Grid item xs={2} sx ={{textAlign:'right', color: '#5D4E99'}}><b>${subtotal.toFixed(1)}</b></Grid>
             </Grid><br/>  
             <Grid container>
                 <Grid item xs={6}>Use Points</Grid>
                 <Grid item xs={6} style ={{textAlign:'right', color: '#707070'}}><small>{discount} point(s)</small></Grid>
             </Grid>
-            <br/>
+            <div style={{textAlign:'right'}}><b style={{fontSize: 12, color:'#5D4E99'}}>Get ${(discount/10).toFixed(1)} off</b></div>
+            <br/><Divider /><br/>  
             <Grid container sx ={{color:'#5D4E99'}}>
                 <Grid item xs={10}> <b>Total</b></Grid>
-                <Grid item xs={2} sx={{textAlign:'right'}}> <b>${(total-discount).toFixed(1)}</b></Grid>
-            </Grid><br/>
-        <div style={{margin: 'auto', textAlign: 'right'}}>
-            <Button 
-                size="large" 
-                //href="/receipt"
-                //onClick={handleReceipt}
-                sx={{border: 2,bgcolor: '#transparent', color: '#5D4E99', ':hover': {borderColor: '#5D4E99', bgcolor: '#5D4E99', color: '#F4CB86'}}}
-                //disabled={!formIsValid()}
-            >
-              Taken
-            </Button>
-        </div>
-        <br/>
+                <Grid item xs={2} sx={{textAlign:'right'}}> <b>${(total).toFixed(1)}</b></Grid>
+            </Grid><br/><br/>
+            <div style={{margin: 'auto', textAlign: 'center'}}>
+                <Button 
+                    size="large" 
+                    onClick={handleTaken}
+                    sx={{border: 2,bgcolor: '#transparent', color: '#5D4E99', ':hover': {borderColor: '#5D4E99', bgcolor: '#5D4E99', color: '#F4CB86'}}}
+                >
+                    Confirm order delivered
+                </Button>
+            </div>
+            <br/>
         </Table>
         </>
-    );}
+    );} else {
+        return(
+            <div>Order taken</div>
+        )
+    }
+    } else return (<p>Loading receipt...</p>)
 }
 
 export {Receipt};
