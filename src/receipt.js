86c0abd849a44b3175efea7df97797a342bfdd54
yylam0 <79801535+yylam0@users.noreply.github.com>
2022-04-08 18:35:56 +0800

    Update receipt.js

--- src/receipt.js
+++ src/receipt.js
@@ -1,4 +1,3 @@
-import Cart from "./cart.json";
 import {UserContext} from './UserContext';
 import {useParams} from 'react-router-dom'
 import DiningIcon from '@mui/icons-material/LocalDining';
@@ -26,7 +25,6 @@ function Receipt() {
     const {user, setUser} = React.useContext(UserContext);
     const [name, setName] = React.useState('');
     const [phone, setPhone] = React.useState('');
-    const [email, setEmail] = React.useState('');
     const [receiptID, setReceiptID] = React.useState('');
     const [res, setRestaurant] = React.useState('');
     const [item, setItem] = React.useState(null);
@@ -40,15 +38,12 @@ function Receipt() {
     const [fetchFinish, setFetch] = React.useState(false);
 
     React.useEffect(()=>{
-        if(fetchFinish== false){
-        fetch("http://localhost:7000/dbReceipt/get" + param.id)
+        fetch("http://localhost:7000/dbReceipt/get/"+window.location.pathname.replace('/receipt/',''))
         .then(res=>res.json())
         .then(data=>{
-            console.log(data[0]);
-            setReceiptID(data[0].receiptID);
+            setReceiptID(data[0].id);
             setRestaurant(data[0].rid);
-            setEmail(data[0].email);
-            setName(data[0].user_name);
+            setName(data[0].ctName);
             setPoint(data[0].point);
             setPhone(data[0].phone);
             setAddress(data[0].address);
@@ -58,16 +53,12 @@ function Receipt() {
             setDiscount(data[0].discount);
             setTotal(data[0].total);
             setStatus(data[0].status);
-            setFetch(true);
         })
-        .catch(err=>{
-          console.log(err);
-          setFetch(false);
-        })}
-    })
+        .then(console.log("Successfully get to receipt info"))
+        .catch(err=>{console.log(err);})
+    },[fetchFinish])
 
-    let param=useParams();
-
+    if (status == false) {
     return (
         <>
         <div style={{width:'80%', margin:'auto'}}>
@@ -80,9 +71,14 @@ function Receipt() {
             </Button>
         </div>
         <br/>
-        <h3 style={{color: '#5D4E99'}}>Your Order {receiptID}</h3>
-        <br/>
+        <h3 style={{color: '#5D4E99'}}>Your Order <span style={{color: '#FFC107'}}>{receiptID}</span></h3><br/>
+        <div style={{textAlign: 'center'}}>
+            <h5 style={{color: '#5D4E99'}}>Name: {name}&nbsp;&nbsp;&nbsp;Tel: {phone}</h5>
+            <h5 style={{color: '#5D4E99'}}>Address: {address}</h5>
+        </div>
+         
         <Table style={{width:'80%', margin:'auto', maxWidth:650}} aria-label="spanning table" padding='normal'>
+            <br/><Divider /><br/> 
             {/*Cart.cartItems.map((item) => (
                 total += item.amount * item.price,
                 <>
@@ -98,90 +94,26 @@ function Receipt() {
                 </Grid>
                 </>
             ))*/}
-            <br/><Divider /><br/>
-            <Box>
-
-                <h4 style ={{color: '#5D4E99'}}>Personal Information</h4>
-                <br/>
-                <Grid container>
-                    <Grid item xs={6}>
-                        <TextField fullWidth required
-                            label="Name"
-                            id="fullWidth"
-                            color="secondary"
-                            value={name}
-                            disabled
-                        />
-                    </Grid>
-                    <Grid item xs={1}/>
-                    <Grid item xs={5}>
-                        <TextField fullWidth required
-                            label="Phone"
-                            id="fullWidth"
-                            color="secondary"
-                            value={phone}
-                            disabled
-                        />
-                    </Grid>
-                </Grid>
-                <br/>
-                <TextField fullWidth required
-                    label="Email"
-                    id="fullWidth"
-                    color="secondary"
-                    value={email}
-                    disabled
-                />
-            </Box>
-            <br/><Divider /><br/>
-            <Box>
-                <h4 style={{color: '#5D4E99'}}>Delivery Address</h4>
-                {address}
-            </Box>
+            <h6><DiningIcon sx={{color:'#5D4E99'}}/>&nbsp;{cutlery? 'Cutlery needed':'Cutlery not needed'}</h6>
             <br/><Divider /><br/>          
             <h4 style={{color: '#5D4E99'}}>Payment</h4>
             <Grid container>
-                <Grid item xs={9}>Members ordering entitlement</Grid>
+                <Grid item xs={9}>Points Rebate</Grid>
                 <Grid item xs={3} style ={{textAlign:'right', color:'#5D4E99'}}>{~~(total/50)*5} Points</Grid>
-            </Grid>
-            <p><small>5 CU EATS Points rebate for every HK$50 net ordering amount of each eligible CUHK email.</small></p>  
+            </Grid><br/>
             <Grid container>
                 <Grid item xs={10}>Subtotal</Grid>
                 <Grid item xs={2} sx ={{textAlign:'right', color: '#5D4E99'}}><b>${total.toFixed(1)}</b></Grid>
-            </Grid>
-            
+            </Grid><br/>  
             <Grid container>
-                <Grid item xs={6}>Your Points</Grid>
-                <Grid item xs={6} style ={{textAlign:'right', color: '#707070'}}><small>{point} valid point(s)</small></Grid>
+                <Grid item xs={6}>Use Points</Grid>
+                <Grid item xs={6} style ={{textAlign:'right', color: '#707070'}}><small>{discount} point(s)</small></Grid>
             </Grid>
-            <TextField fullWidth 
-                    label="Use Points"
-                    autoComplete="off"
-                    id="fullWidth"
-                    variant="standard"
-                    value={point}
-                    sx={{mt:1}}
-                    color="secondary"
-                    inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-                    disabled
-            />
-            <b style={{fontSize: 12, color:'#5D4E99'}}>Get ${discount.toFixed(1)} off</b>
-            <br/><br/>
+            <br/>
             <Grid container sx ={{color:'#5D4E99'}}>
                 <Grid item xs={10}> <b>Total</b></Grid>
                 <Grid item xs={2} sx={{textAlign:'right'}}> <b>${(total-discount).toFixed(1)}</b></Grid>
-            </Grid>
-            <br/><Divider /><br/>
-            <FormGroup>
-            <Grid container>
-                <Grid item xs={11}>
-                    <h6><DiningIcon sx={{color:'#5D4E99'}}/> Do you need cutlery?</h6>
-                </Grid>
-                <Grid item xs={1} sx={{textAlign:'right'}}>
-                </Grid>
-            </Grid>
-        </FormGroup>
-        <br/>
+            </Grid><br/>
         <div style={{margin: 'auto', textAlign: 'right'}}>
             <Button 
                 size="large" 
@@ -196,7 +128,7 @@ function Receipt() {
         <br/>
         </Table>
         </>
-    );
+    );}
 }
 
 export {Receipt};
