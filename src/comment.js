47b6085392947f6f19f6a29fa96e280001cea4f3
Blackawabi <paucw0925@outlook.com>
2022-04-12 23:57:45 +0800

    guest cant comment now

--- src/comment.js
+++ src/comment.js
@@ -594,7 +594,7 @@ function AddComment(){
       
     return(
         <div>
-            <Fab color="secondary" sx={{position: 'fixed', bottom: 32 ,right:32,zIndex:10000}} onClick={handleClick} hidden={localStorage.getItem('type') == "admin"}>
+            <Fab color="secondary" sx={{position: 'fixed', bottom: 32 ,right:32,zIndex:10000}} onClick={handleClick} hidden={localStorage.getItem('type') == "admin"||localStorage.getItem('type') == "guest"}>
                 <AddIcon />
             </Fab>
             <Dialog component={"div"} width="md"height="md" open={open} onClose={handleClose} scroll="paper" style={{zIndex:9999, overflowY:"visible",position:"absolute"}} fullWidth>
