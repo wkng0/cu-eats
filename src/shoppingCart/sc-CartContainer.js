884f6eaf8e891fd9d6bc7adafca4c5c0458c0608
HillaryTam <hillarytam1024@gmail.com>
2022-04-13 21:41:54 +0800

    fix + delete files

--- src/shoppingCart/sc-CartContainer.js
+++ src/shoppingCart/sc-CartContainer.js
@@ -32,7 +32,7 @@ const CartItem = ({ id,img ,title, variant,price,amount }) => {
         </div>
         <div>
           {/* increase amount */}
-          <button className='sc-amount-btn' disabled={amount===2} onClick={() => increase({id:id,variant:variant}) }>
+          <button className='sc-amount-btn' disabled={amount===5} onClick={() => increase({id:id,variant:variant}) }>
             <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
               <path d='M10.707 7.05L10 6.343 4.343 12l1.414 1.414L10 9.172l4.243 4.242L15.657 12z' />
             </svg>
